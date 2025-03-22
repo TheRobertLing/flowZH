e8384e31dee9adbc673742167375f618d7a62539
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-23 00:41:50 +1100

    added navbar and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import NavigationBarComponent from './components/NavigationBarComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 </script>
 
 <template>
+  <NavigationBarComponent></NavigationBarComponent>
   <RouterView></RouterView>
+  <FooterComponent></FooterComponent>
 </template>
 
 <style scoped>
