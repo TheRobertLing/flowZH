c1f058f1f88f90839333757917d69f568d785d1f
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-23 22:34:43 +1100

    added stub for resources

--- src/views/ResourcesPageView.vue
+++ src/views/ResourcesPageView.vue
@@ -3,9 +3,31 @@
 </script>
 
 <template>
-  <div>
 
+  <div class="bg-base-200 flex justify-center gap-8">
+    <!-- Left Sidebar for future content-->
+    <!-- <div class="border grow-1 min-h-screen">
+      Left
+    </div> -->
+
+    <!-- Main Content -->
+    <div class="flex flex-col max-w-352 items-center bg-base-300 grow-3 min-h-screen text-center mt-8 mb-8 rounded-xl">
+      <h2 class="text-xl font-semibold mt-8 mb-8 select-none">ZiFlow's Resources Page</h2>
+
+      <!-- Resource List -->
+      <div class="grid-container grid gap-8 mb-8 max-w-7xl">
+        Hello I am under the water
+      </div>
+
+
+    </div>
+
+    <!-- Right Sidebar for future content -->
+    <!-- <div class="border grow-1 min-h-screen">
+      Right
+    </div> -->
   </div>
+
 </template>
 
 <style scoped>
