f2b3cac29552d8f375104a0dc832c334d42a2a5e
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-23 22:34:03 +1100

    refactored unnecessary routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,56 @@
-import HomeView from '@/views/HomeView.vue'
+import AboutPageView from '@/views/AboutPageView.vue'
+import ChangeLogPageView from '@/views/ChangeLogPageView.vue'
+import GamesPageView from '@/views/GamesPageView.vue'
+import HomePageView from '@/views/HomePageView.vue'
+import PageNotFoundView from '@/views/PageNotFoundView.vue'
 import PinyinTypeSettingsView from '@/views/pinyin-typer/PinyinTypeSettingsView.vue'
-import PinyinTypeView from '@/views/pinyin-typer/PinyinTypeView.vue'
-import WelcomeView from '@/views/WelcomeView.vue'
+import PinyinTyperView from '@/views/pinyin-typer/PinyinTyperView.vue'
+import ResourcesPageView from '@/views/ResourcesPageView.vue'
+import RoadMapView from '@/views/RoadMapView.vue'
+
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Welcome', component: WelcomeView },
-    { path: '/home', name: 'Home', component: HomeView },
+    // Home Route
+    { path: '/', name: 'HomePage', component: HomePageView },
+
+
+
+
+    // Game Routes
+    { path: '/games', name: 'GamesPage', component: GamesPageView },
     {
-      path: '/pinyin',
+      path: '/pinyin-typer',
       name: 'PinyinTyperGame',
-      component: PinyinTypeView,
+      component: PinyinTyperView,
     },
     {
-      path: '/pinyin/settings',
+      path: '/pinyin-typer/settings',
       name: 'PinyinTyperSettings',
       component: PinyinTypeSettingsView,
     },
+
+
+
+    // Resource Routes
+    { path: '/resources', name: 'ResourcesPage', component: ResourcesPageView },
+
+
+
+
+    // Other Routes
+    { path: '/about', name: 'AboutPage', component: AboutPageView },
+    { path: '/changelog', name: 'ChangeLogPage', component: ChangeLogPageView },
+    { path: '/roadmap', name: 'RoadMapPage', component: RoadMapView },
+
+
+
+
+    // Page not found
+    { path: '/page-not-found', name: 'PageNotFoundPage', component: PageNotFoundView },
+    { path: '/:pathMatch(.*)*', redirect: { name: 'PageNotFoundPage' } },
   ],
 })
 
