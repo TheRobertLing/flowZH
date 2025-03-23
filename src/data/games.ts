9806a94db3a912bf7295dc922d5b80be53150a66
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-23 22:32:35 +1100

    added game meta data file

---
+++ src/data/games.ts
@@ -0,0 +1,17 @@
+// Meta information for ZiFlow games
+import type { GameCardData } from '@/types'
+
+export const gameMetaInfoList: GameCardData[] = [
+  {
+    // imgPath // Comment out for now, don't have an img yet, use default
+    viewName: 'PinyinTyperGame',
+    name: 'Pinyin Typer',
+    description: 'Practise your Pinyin recognitions skills.',
+    categories: ['Pinyin', 'Typing'],
+  },
+
+  // Empty games, make the site look a bit fuller for now
+  { viewName: 'PageNotFoundPage' },
+  { viewName: 'PageNotFoundPage' },
+  { viewName: 'PageNotFoundPage' },
+]
