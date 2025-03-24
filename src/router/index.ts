ab2cf689fe9c4a93239b1f4be7aa33256a8d3aba
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-25 02:14:40 +1100

    linted

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,6 @@ import ChangeLogPageView from '@/views/ChangeLogPageView.vue'
 import GamesPageView from '@/views/GamesPageView.vue'
 import HomePageView from '@/views/HomePageView.vue'
 import PageNotFoundView from '@/views/PageNotFoundView.vue'
-import PinyinTypeSettingsView from '@/views/pinyin-typer/PinyinTypeSettingsView.vue'
 import PinyinTyperView from '@/views/pinyin-typer/PinyinTyperView.vue'
 import ResourcesPageView from '@/views/ResourcesPageView.vue'
 import RoadMapView from '@/views/RoadMapView.vue'
@@ -17,9 +16,6 @@ const router = createRouter({
     // Home Route
     { path: '/', name: 'HomePage', component: HomePageView },
 
-
-
-
     // Game Routes
     { path: '/games', name: 'GamesPage', component: GamesPageView },
     {
@@ -27,34 +23,25 @@ const router = createRouter({
       name: 'PinyinTyperGame',
       component: PinyinTyperView,
     },
-    {
-      path: '/pinyin-typer/settings',
-      name: 'PinyinTyperSettings',
-      component: PinyinTypeSettingsView,
-    },
-
-
 
     // Resource Routes
     { path: '/resources', name: 'ResourcesPage', component: ResourcesPageView },
 
-
     // Article Routes
     { path: '/articles', name: 'ArticlesPage', component: ArticlesPageView },
-
 
     // Other Routes
     { path: '/about', name: 'AboutPage', component: AboutPageView },
     { path: '/changelog', name: 'ChangeLogPage', component: ChangeLogPageView },
     { path: '/roadmap', name: 'RoadMapPage', component: RoadMapView },
 
-
-
-
     // Page not found
     { path: '/page-not-found', name: 'PageNotFoundPage', component: PageNotFoundView },
     { path: '/:pathMatch(.*)*', redirect: { name: 'PageNotFoundPage' } },
   ],
+  scrollBehavior() {
+    return { top: 0 }
+  },
 })
 
 export default router
