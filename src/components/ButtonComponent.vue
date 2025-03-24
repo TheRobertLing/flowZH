f8714d0385bd3ad874466f874a1a44380d96a2dc
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-25 02:14:13 +1100

    changed css themeing

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -5,7 +5,7 @@ const emit = defineEmits(['buttonClick'])
 <template>
   <button
     @click="emit('buttonClick')"
-    class="btn btn-primary btn-xs sm:btn-sm md:btn-md border-2 border-solid rounded-xl hover:text-green-500"
+    class="btn btn-neutral btn-xs sm:btn-sm md:btn-md border-2 border-solid rounded-xl hover:text-green-500"
   >
     <slot> Click Me </slot>
   </button>
