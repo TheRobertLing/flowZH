638c46c29d04c2947ee8167efb12fa4dea8557aa
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-26 17:33:35 +1100

    changed routes to lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,46 +1,53 @@
-import AboutPageView from '@/views/AboutPageView.vue'
-import ChangeLogPageView from '@/views/ChangeLogPageView.vue'
-import GamesPageView from '@/views/GamesPageView.vue'
-import HomePageView from '@/views/HomePageView.vue'
-import PageNotFoundView from '@/views/PageNotFoundView.vue'
-import PinyinTyperView from '@/views/pinyin-typer/PinyinTyperView.vue'
-import ResourcesPageView from '@/views/ResourcesPageView.vue'
-import RoadMapView from '@/views/RoadMapView.vue'
-
 import { createRouter, createWebHistory } from 'vue-router'
-import ArticlesMenuView from '@/views/ArticlesMenuView.vue'
-import ArticlesSingleView from '@/views/ArticlesSingleView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     // Home Route
-    { path: '/', name: 'HomePage', component: HomePageView },
+    { path: '/', name: 'HomePage', component: () => import('@/views/HomePageView.vue') },
 
     // Game Routes
-    { path: '/games', name: 'GamesPage', component: GamesPageView },
+    { path: '/games', name: 'GamesPage', component: () => import('@/views/GamesPageView.vue') },
     {
       path: '/pinyin-typer',
       name: 'PinyinTyperGame',
-      component: PinyinTyperView,
+      component: () => import('@/views/pinyin-typer/PinyinTyperView.vue'),
     },
 
     // Resource Routes
-    { path: '/resources', name: 'ResourcesPage', component: ResourcesPageView },
+    {
+      path: '/resources',
+      name: 'ResourcesPage',
+      component: () => import('@/views/ResourcesPageView.vue'),
+    },
 
     // Article Routes
-    { path: '/articles', name: 'ArticlesMenuPage', component: ArticlesMenuView },
-
-    // Implement Later
-    { path: '/articles/:slug', name: 'ArticlesSinglePage', component: ArticlesSingleView },
+    {
+      path: '/articles',
+      name: 'ArticlesMenuPage',
+      component: () => import('@/views/ArticlesMenuView.vue'),
+    },
+    {
+      path: '/articles/:slug',
+      name: 'ArticlesSinglePage',
+      component: () => import('@/views/ArticlesSingleView.vue'),
+    },
 
     // Other Routes
-    { path: '/about', name: 'AboutPage', component: AboutPageView },
-    { path: '/changelog', name: 'ChangeLogPage', component: ChangeLogPageView },
-    { path: '/roadmap', name: 'RoadMapPage', component: RoadMapView },
+    { path: '/about', name: 'AboutPage', component: () => import('@/views/AboutPageView.vue') },
+    {
+      path: '/changelog',
+      name: 'ChangeLogPage',
+      component: () => import('@/views/ChangeLogPageView.vue'),
+    },
+    { path: '/roadmap', name: 'RoadMapPage', component: () => import('@/views/RoadMapView.vue') },
 
     // Page not found
-    { path: '/page-not-found', name: 'PageNotFoundPage', component: PageNotFoundView },
+    {
+      path: '/page-not-found',
+      name: 'PageNotFoundPage',
+      component: () => import('@/views/PageNotFoundView.vue'),
+    },
     { path: '/:pathMatch(.*)*', redirect: { name: 'PageNotFoundPage' } },
   ],
   scrollBehavior() {
